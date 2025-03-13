ea6616786dcaceaaa77ff8ace1ec6a944818c9db
Mir Shakir <brainlessanime123@gmail.com>
2025-03-13 13:03:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "codeflix_bots"
+API_ID       = "29191109"
+API_HASH     = "cac582c764755531d7fde92e444a23b7"
+BOT_TOKEN    = "7667583520:AAGuEba3p8d7L3H691600mBFfEiRNzFtglE"
+SESSION      = "tUx0IRG+nzT7Qyuq1p8LC8m4d0syvzQyFJL1WEXeP10E2nlBFehTJ6uyL/if70akCM6MDO8cCPxo4Zt0a0tjzgOJHV4lsg2Kedsf5c9g1HqfnM/il0JBtReaHi6s33aIVySIquVf+EOuXm0qCRkA1xnNKIphKFFOElcdBtXiVdHuj+eziHKzxZNxd30433V8d3Blenlw"
+DATABASE_URI = "mongodb+srv://brainless:Solocrazy123@cluster0.uymhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = "-1002187936288"
+ADMIN        = "7514451812"
+CHANNEL      = "cypherixsocity"
